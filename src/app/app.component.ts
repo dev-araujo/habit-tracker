c25e5856b5126671cb98b9dc8413a677b428e198
dev-araujo <theriancirculo@protonmail.com>
2024-04-24 16:44:13 -0300

    feat: export component header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HabitsComponent } from './views/habits/habits.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HabitsComponent],
+  imports: [CommonModule, RouterOutlet, HabitsComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
