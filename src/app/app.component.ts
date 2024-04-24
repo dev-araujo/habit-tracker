fcc241742adb62db9aea6c9e75958b3a211b3fcc
dev-araujo <theriancirculo@protonmail.com>
2024-04-24 16:41:16 -0300

    feat: create base header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HabitsComponent } from './views/habits/habits.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HabitsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'habit-tracker';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { HabitsComponent } from './views/habits/habits.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: HabitsComponent }];
