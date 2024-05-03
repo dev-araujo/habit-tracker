a6ec59b2f1a66c2bf5e582d45cd015d7c0e0878a
dev-araujo <theriancirculo@protonmail.com>
2024-05-03 11:37:48 -0300

    fix: database config

--- back/config/database.js
+++ back/config/database.js
@@ -1,13 +1,29 @@
 const sqlite3 = require("sqlite3").verbose();
 
 const db = new sqlite3.Database(
-  "./habits.db",
+  "./database/habits.db",
   sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
   (err) => {
     if (err) {
-      console.error(err.message);
+      console.error("Erro ao conectar ao banco de dados SQLite:", err.message);
+      return;
     }
-    console.log("Connected to the SQLite database.");
+    console.log("Conectado ao banco de dados SQLite.");
+
+    db.run(
+      `CREATE TABLE IF NOT EXISTS habits (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      days TEXT
+    )`,
+      (err) => {
+        if (err) {
+          console.error("Erro ao criar a tabela 'habits':", err.message);
+        } else {
+          console.log("Tabela 'habits' criada ou já existente.");
+        }
+      }
+    );
   }
 );
 
